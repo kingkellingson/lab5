79c882ce206b0adcdcca0ae95fe5f701f559a777
kyle <kingkellingson@gmail.com>
2021-08-28 21:55:46 -0600

    reorder

--- front-end/src/views/Photo.vue
+++ front-end/src/views/Photo.vue
@@ -19,25 +19,17 @@ export default {
       photo: '',
     }
   },
-//   props: {
-//     selectedPhoto: Object,
-//   },
-  created() {
-    this.id = this.$route.params.id;
-    console.log("you have created: ", this.path);
-    this.photo = getPhoto();
-    // this.random =
-    //   Math.floor(Math.random() * 90) + 10;
-    // this.amount = this.random * parseFloat(this.product.price.replace(/\$|,/g, ''));
-    // this.amount = this.amount.toFixed(2);
-  },
   methods: {
       async getPhoto() {
         console.log("called Get Photo")
         let photo = await axios.get("/api/photos/"+this.id);
         return photo;
-      }
-      
-  }
+      }  
+  },
+  created() {
+    this.id = this.$route.params.id;
+    console.log("you have created: ", this.path);
+    this.photo = getPhoto();
+  },
 }
 </script>
