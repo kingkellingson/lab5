8c277104566875270fd899739f0c6fe9f06d5686
kyle <kingkellingson@gmail.com>
2021-08-28 23:01:20 -0600

    added styles

--- front-end/src/views/Photo.vue
+++ front-end/src/views/Photo.vue
@@ -6,11 +6,11 @@
   <div class="image">
     <img :src='photos.data[0].path' alt="no image due to error">
   </div>
-  <!-- <div class="photoInfo">
+  <div class="photoInfo">
         <p class="photoTitle">{{photos.data[0].title}}</p>
         <p class="photoName">{{photos.data[0].user.firstName}} {{photos.data[0].user.lastName}}</p>
       </div>
-    <p class="photoDate">{{formatDate(photo.created)}}</p> -->
+    <p class="photoDate">{{formatDate(photo.created)}}</p>
 </div>
 </template>
 
@@ -55,6 +55,25 @@ export default {
 .image img{
     width: 100%;
     object-fit:cover;
+}
+
+.photoInfo {
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.8em;
+}
+
+.photoInfo p {
+  margin: 0px;
+}
+
+.photoDate {
+  font-size: 0.7em;
+  font-weight: normal;
+}
+
+p {
+  margin: 0px;
 }
 
 </style>
