729b55e8e077782f56666da350cebeb5c0e4024a
kyle <kingkellingson@gmail.com>
2021-08-28 22:15:47 -0600

    plain old photo.path

--- front-end/src/views/Photo.vue
+++ front-end/src/views/Photo.vue
@@ -4,7 +4,7 @@
     <p>Congratulations, you have ordered {{id}}</p>
   </div>
   <div class="image">
-    <img :src="path" alt="no image due to error">
+    <img :src="photo.path" alt="no image due to error">
   </div>
 </div>
 </template>
@@ -22,17 +22,18 @@ export default {
   },
   methods: {
       async getPhoto() {
-        let photo = await axios.get("/api/photos/"+this.id);
-        console.log("Returned Photo: ", photo);
-        let path = photo.data[0].path;
-        console.log("data path: ", photo.data[0].path);
-        return path;
+        this.photo = await axios.get("/api/photos/"+this.id);
+        console.log("Returned Photo: ", this.photo);
+        // let path = photo.data[0].path;
+        // console.log("data path: ", photo.data[0].path);
+        // return path;
       }  
   },
   created() {
     this.id = this.$route.params.id;
     console.log("you have created: ", this.id);
-    this.path = this.getPhoto();
+    this.getPhoto();
+    // this.path = this.getPhoto();
     
   },
 }
