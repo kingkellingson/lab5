0c3062e898932aee0d7bdeea3ded83cad35c2039
kyle <kingkellingson@gmail.com>
2021-08-28 22:07:00 -0600

    condense to path

--- front-end/src/views/Photo.vue
+++ front-end/src/views/Photo.vue
@@ -4,7 +4,7 @@
     <p>Congratulations, you have ordered {{id}}</p>
   </div>
   <div class="image">
-    <img :src="photo.data[0].path" alt="no image due to error">
+    <img :src="path" alt="no image due to error">
   </div>
 </div>
 </template>
@@ -17,12 +17,14 @@ export default {
     return {
       id: '',
       photo: '',
+      path: '',
     }
   },
   methods: {
       async getPhoto() {
         let photo = await axios.get("/api/photos/"+this.id);
         console.log("Returned Photo: ", photo)
+        path = photo.data[0].path,
         console.log("data path: ", photo.data[0].path);
         return photo;
       }  
