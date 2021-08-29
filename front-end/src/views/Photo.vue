38cdbc684167c34f431db4135407d0c6cd2f6484
kyle <kingkellingson@gmail.com>
2021-08-28 21:54:33 -0600

    import axios debug

--- front-end/src/views/Photo.vue
+++ front-end/src/views/Photo.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'Photo',
   data() {
@@ -23,8 +24,8 @@ export default {
 //   },
   created() {
     this.id = this.$route.params.id;
-    console.log("you have created: ", this.path)
-    getPhoto()
+    console.log("you have created: ", this.path);
+    this.photo = getPhoto();
     // this.random =
     //   Math.floor(Math.random() * 90) + 10;
     // this.amount = this.random * parseFloat(this.product.price.replace(/\$|,/g, ''));
@@ -32,9 +33,9 @@ export default {
   },
   methods: {
       async getPhoto() {
-        let photo = await axios.get("/api/photos/"+id);
-        this.photo = photo;
         console.log("called Get Photo")
+        let photo = await axios.get("/api/photos/"+this.id);
+        return photo;
       }
       
   }
