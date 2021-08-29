6ab7d68cfe3ff26006b4238f0aa04b903c01b23a
kyle <kingkellingson@gmail.com>
2021-08-28 22:25:47 -0600

    v-bind

--- front-end/src/views/Photo.vue
+++ front-end/src/views/Photo.vue
@@ -4,7 +4,7 @@
     <p>Congratulations, you have ordered {{id}}</p>
   </div>
   <div class="image">
-    <img :src="this.path" alt="no image due to error">
+    <img :src='this.path' alt="no image due to error">
   </div>
 </div>
 </template>
@@ -32,7 +32,8 @@ export default {
   created() {
     this.id = this.$route.params.id;
     this.path = this.$route.params.path;
-    // console.log("you have created: ", this.id);
+    console.log("you have created: ", this.id);
+    console.log("with path:", this.path);
     this.getPhoto();
     // this.path = this.getPhoto();
     
