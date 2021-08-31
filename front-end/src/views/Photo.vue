b8e3fe26af54399c2713ddda0014a16b625e8c1c
kyle <kingkellingson@gmail.com>
2021-08-31 10:18:54 -0600

    just comment

--- front-end/src/views/Photo.vue
+++ front-end/src/views/Photo.vue
@@ -37,7 +37,7 @@
           comment added!
             <h3>Added {{formatDate(comment.created)}}</h3>
             <p><i>{{comment.words}}</i></p>
-            <p>-- {{comment.user}} {{commment.user}}</p>
+            <p>-- {{comment}} {{commment}}</p>
 
         </div>
     </div>
